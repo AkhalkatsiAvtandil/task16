4a52436375691098b8db4f4822ee5c59123b0d01
AkhalkatsiAvtandil <akhalkatsiavto@gmail.com>
2026-05-04 18:05:12 +0400

    დავალება პირველი
    გამოვთვალეთ საშუალო არითმეტიკული

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+// console.log("hello world");
+const myArray = [1121, 1453, 1071, 1203, 1195, 1556];
+const myArraysum =
+  myArray[0] + myArray[1] + myArray[2] + myArray[3] + myArray[4] + myArray[5];
+
+console.log(myArraysum);
+
+const myArrayarithmeticmean = myArraysum / myArray.length;
+console.log(myArrayarithmeticmean);
